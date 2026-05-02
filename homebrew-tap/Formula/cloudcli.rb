3b8382e9217463432c580a15dec32de8bf031b12
saqlainansari <saqlainzarjisansari@gmail.com>
2026-05-02 16:07:46 +0530

    fix: update Homebrew formula with correct SHA256 and URL

--- homebrew-tap/Formula/cloudcli.rb
+++ homebrew-tap/Formula/cloudcli.rb
@@ -1,7 +1,8 @@
 class Cloudcli < Formula
   desc "Multi-Database Backup CLI Tool - Backup to the cloud with one command"
-  homepage "https://github.com/Mysterious786/cloudcli"
-  url "https://github.com/Mysterious786/cloudcli/releases/latest/download/cloudcli.jar"
+  homepage "https://github.com/Mysterious786/CloudCLI"
+  url "https://github.com/Mysterious786/CloudCLI/releases/download/v1.0.0/cloudcli.jar"
+  sha256 "9edc2a1ac29b43dcf0318e2589729c20614e4d7088bcf8260e0f16215d25acb6"
   version "1.0.0"
   license "MIT"
 
@@ -9,7 +10,7 @@ class Cloudcli < Formula
 
   def install
     libexec.install "cloudcli.jar"
-    
+
     (bin/"cloudcli").write <<~EOS
       #!/bin/bash
       exec "#{Formula["openjdk@17"].opt_bin}/java" -jar "#{libexec}/cloudcli.jar" "$@"
@@ -17,6 +18,6 @@ class Cloudcli < Formula
   end
 
   test do
-    system "#{bin}/cloudcli", "--version"
+    assert_match "cloudcli", shell_output("#{bin}/cloudcli --version 2>&1", 0)
   end
 end
